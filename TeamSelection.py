934e7531b3511852e0bcd4289e7184bce01beff9
thelexmeister <88957860+thelexmeister@users.noreply.github.com>
2025-01-05 15:50:55 -0700

    Update TeamSelection.py

--- TeamSelection.py
+++ TeamSelection.py
@@ -92,10 +92,10 @@ with st.sidebar:
             # Display each player's price in the sidebar
             st.sidebar.write(f"<span style='color:blue; font-size:16px; display:inline;'>{player}: </span><span style='font-size:16px; display:inline;'>${price}</span>", unsafe_allow_html=True)
 
-            if total_price > 12000:
-                st.sidebar.markdown(f"<h3 style='color:red;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
-            else:
-                st.sidebar.markdown(f"<h3 style='color:green;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
+if total_price > 12000:
+    st.sidebar.markdown(f"<h3 style='color:red;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
+else:
+    st.sidebar.markdown(f"<h3 style='color:green;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
             
 
 
