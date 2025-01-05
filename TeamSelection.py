e2fa595d737d132f0440a70d942f12042c61e326
thelexmeister <88957860+thelexmeister@users.noreply.github.com>
2025-01-05 14:34:34 -0700

    Update TeamSelection.py

--- TeamSelection.py
+++ TeamSelection.py
@@ -59,7 +59,7 @@ for player in selected_players:
         # Retrieve the price if the player exists
         price = player_row['Price'].values[0]
         total_price += price
-        st.write(f"<h3 style='color:blue;'> {player}:</h3> ${price}", unsafe_allow_html=True)
+        st.write(f"<h2 style='color:blue;'> {player}:</h2> ${price}", unsafe_allow_html=True)
 
 # Display the total price with color based on condition
 if total_price > 12000:
