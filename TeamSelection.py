11600388c658455e6b4bade3e7ae456fc5246bf6
thelexmeister <88957860+thelexmeister@users.noreply.github.com>
2025-01-05 15:05:10 -0700

    Update TeamSelection.py

--- TeamSelection.py
+++ TeamSelection.py
@@ -51,10 +51,10 @@ with col2:
     selected_players = [qb, rb1, rb2, wr1, wr2, te] + flex + [k, dst]
 
 # Display the selected players
-st.write("Your selected players:")
-st.write(" ")
+st.sidebar.write("Your selected players:")
+st.sidebar.write(" ")
 #st.write("<h5 style='color:blue;'> Team Name:</h5> ", team_name, unsafe_allow_html=True)
-st.write(f"<span style='color:red; font-size:16px; display:inline;'>Team Name: </span><span style='font-size:16px; display:inline;'>{team_name}</span>", unsafe_allow_html=True)
+st.sidebar.write(f"<span style='color:red; font-size:16px; display:inline;'>Team Name: </span><span style='font-size:16px; display:inline;'>{team_name}</span>", unsafe_allow_html=True)
 # Calculate and display the price of the selected players
 total_price = 0
 for player in selected_players:
