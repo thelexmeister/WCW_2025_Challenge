84420a2bb2fa8599abddfb209fadc89353587c98
thelexmeister <88957860+thelexmeister@users.noreply.github.com>
2025-01-05 15:03:43 -0700

    Update TeamSelection.py

--- TeamSelection.py
+++ TeamSelection.py
@@ -63,14 +63,14 @@ for player in selected_players:
 
     # Check if the player exists in the filtered dataframe
     if player_row.empty:
-        st.warning(f"Player '{player}' not found in the filtered list.")
+        st.sidebar.warning(f"Player '{player}' not found in the filtered list.")
     else:
         # Retrieve the price if the player exists
         price = player_row['Price'].values[0]
         total_price += price
-        st.write(f"<span style='color:blue; font-size:16px; display:inline;'>{player}: </span><span style='font-size:16px; display:inline;'>${price}</span>", unsafe_allow_html=True)
+        st.sidebar.write(f"<span style='color:blue; font-size:16px; display:inline;'>{player}: </span><span style='font-size:16px; display:inline;'>${price}</span>", unsafe_allow_html=True)
 # Display the total price with color based on condition
 if total_price > 12000:
-    st.markdown(f"<h3 style='color:red;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h3 style='color:red;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
 else:
-    st.markdown(f"<h3 style='color:green;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
+    st.sidebar.markdown(f"<h3 style='color:green;'>Total Price: ${total_price}</h3>", unsafe_allow_html=True)
